a25f214c86de4f974a3524077839b84d966311cc
jdgilbert <joadgilbert@hotmail.com>
2013-09-17 17:08:24 -0500

    initial

---
+++ Shedjule/Shedjule/Models/SJContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shedjule.Models
+{
+    public class SJContext : DbContext
+    {
+        public SJContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
